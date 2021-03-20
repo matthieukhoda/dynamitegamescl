b1d968c6df5f6023f93259bcbf344526de59d00c
matthieukhoda <matthieu.khodabuccus1@gmail.com>
2021-03-20 21:54:27 +0100

    visuel en cours

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,13 +2,101 @@
 
 namespace App\Controller;
 
-class DefaultController
+use App\Entity\Games;
+use App\Form\GamesType;
+use App\Repository\CategoryRepository;
+use App\Repository\GamesRepository;
+use App\Repository\PlateformRepository;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/front")
+ */
+class DefaultController extends AbstractController
 {
-
-    public function index()
+    /**
+     * @Route("/", name="games_index", methods={"GET"})
+     */
+    public function index(GamesRepository $gamesRepository, CategoryRepository $categoryRepository, PlateformRepository $plateformRepository): Response
     {
+        $games = $gamesRepository->findAll();
 
-        var_dump("ok");
-        die();
+        return $this->render('front/index.html.twig',  [
+            'games' => $games,
+
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="games_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $game = new Games();
+        $form = $this->createForm(GamesType::class, $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $game->setUpdatedAt(new DateTime());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($game);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('games_index');
+        }
+
+        return $this->render('games/new.html.twig', [
+            'game' => $game,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="games_show", methods={"GET"})
+     */
+    public function show(Games $game): Response
+    {
+        return $this->render('games/show.html.twig', [
+            'game' => $game,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="games_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Games $game): Response
+    {
+        $form = $this->createForm(GamesType::class, $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $game->setUpdatedAt(new DateTime());
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('games_index');
+        }
+
+        return $this->render('games/edit.html.twig', [
+            'game' => $game,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="games_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Games $game): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $game->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($game);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('games_index');
     }
 }
---
+++ src/Entity/Default.php
@@ -0,0 +1,289 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GamesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use FFI;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity(repositoryClass=GamesRepository::class)
+ * @Vich\Uploadable
+ */
+class Games
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $stock;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $eanCode;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Category::class, mappedBy="games")
+     */
+    private $categories;
+
+
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Plateform::class, mappedBy="games")
+     */
+    private $plateforms;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="game", orphanRemoval=true)
+     */
+    private $comments;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @Vich\UploadableField(mapping="article_cover", fileNameProperty="imageName")
+     */
+    private $imageFile;
+
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+        $this->plateforms = new ArrayCollection();
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getEanCode(): ?int
+    {
+        return $this->eanCode;
+    }
+
+    public function setEanCode(?int $eanCode): self
+    {
+        $this->eanCode = $eanCode;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+
+
+
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+            $category->addGame($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removeGame($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Plateform[]
+     */
+    public function getPlateforms(): Collection
+    {
+        return $this->plateforms;
+    }
+
+    public function addPlateform(Plateform $plateform): self
+    {
+        if (!$this->plateforms->contains($plateform)) {
+            $this->plateforms[] = $plateform;
+            $plateform->addGame($this);
+        }
+
+        return $this;
+    }
+
+    public function removePlateform(Plateform $plateform): self
+    {
+        if ($this->plateforms->removeElement($plateform)) {
+            $plateform->removeGame($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setGame($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getGame() === $this) {
+                $comment->setGame(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+
+    public function __toString()
+    {
+        return $this->title;
+    }
+}
