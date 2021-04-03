bb9c8652c083f5597d04291c3273fe0bcf9d1ca1
matthieukhoda <matthieu.khodabuccus1@gmail.com>
2021-04-03 15:57:25 +0200

    presque fini

---
+++ src/Form/ContactType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fullName', TextType::class, [
+                'label' => 'İsim',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'E-posta'
+            ]);
+        // ->add('message', TextareaType::class, [
+        //     'attr' => ['rows' => 5],
+        // ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
